9523722f8f8c9a64f9d5cfeece2ed525b0deea64
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-11-12 13:14:12 +0100

    top bar added

---
+++ lib/widgets/tab_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:tp1/screens/basket_screen.dart';
+import 'package:tp1/screens/home_srcreen.dart';
+import 'package:tp1/screens/library_screen.dart';
+
+class MyTabBar extends StatefulWidget {
+  const MyTabBar({super.key});
+
+  @override
+  State<MyTabBar> createState() => _MyTabBarState();
+}
+
+class _MyTabBarState extends State<MyTabBar> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  final List<Widget> pages = [HomeScreen(), LibraryScreen(), BasketScreen()];
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: pages.length, vsync: this);
+
+    // Mettre à jour l'état quand l'onglet change
+    tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 33, 107, 235),
+        title: const Text(
+          "Store INSAT",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        bottom: TabBar(
+          controller: tabController,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          tabs: const [
+            Tab(icon: Icon(Icons.home_outlined), text: "Home"),
+            Tab(icon: Icon(Icons.bookmark_outline), text: "Library"),
+            Tab(icon: Icon(Icons.shopping_bag), text: "Basket"),
+          ],
+        ),
+      ),
+      body: pages[tabController.index],
+    );
+  }
+}
