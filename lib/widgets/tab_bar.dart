36e35f5c172158bfb9a7bc287a1714c6814e20f6
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-11-19 20:28:19 +0100

    fixed problem in tabbar

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -3,52 +3,30 @@ import 'package:tp1/screens/basket_screen.dart';
 import 'package:tp1/screens/library_screen.dart';
 import 'package:tp1/screens/auth/signup_page.dart';
 
-class MyTabBar extends StatefulWidget {
+class MyTabBar extends StatelessWidget {
   const MyTabBar({super.key});
 
   @override
-  State<MyTabBar> createState() => _MyTabBarState();
-}
-
-class _MyTabBarState extends State<MyTabBar> with TickerProviderStateMixin {
-  late TabController tabController;
-
-  final List<Widget> pages = [SignUpPage(), LibraryScreen(), BasketScreen()];
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: pages.length, vsync: this);
-
-    // Mettre à jour l'état quand l'onglet change
-    tabController.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 144, 171, 218),
-        bottom: TabBar(
-          controller: tabController,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.white60,
-          tabs: const [
-            Tab(icon: Icon(Icons.person_outline), text: "Sign Up"),
-            Tab(icon: Icon(Icons.bookmark_outline), text: "Library"),
-            Tab(icon: Icon(Icons.shopping_bag), text: "Basket"),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 144, 171, 218),
+          bottom: const TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white60,
+            tabs: [
+              Tab(icon: Icon(Icons.person_outline), text: "Sign Up"),
+              Tab(icon: Icon(Icons.bookmark_outline), text: "Library"),
+              Tab(icon: Icon(Icons.shopping_bag), text: "Basket"),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [SignUpPage(), LibraryScreen(), BasketScreen()],
         ),
       ),
-      body: TabBarView(controller: tabController, children: pages),
     );
   }
 }
